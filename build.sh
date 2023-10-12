229351a8c4d5c0348f090fa68f641f6fea2064fa
diggymo <marooon88@gmail.com>
2023-10-12 11:26:43 +0900

    READMEにbuild.shを記述

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ set -e
 
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ../blog/static/wasm-lifegame --target web ./target/wasm32-unknown-unknown/release/lifegame.wasm
-
